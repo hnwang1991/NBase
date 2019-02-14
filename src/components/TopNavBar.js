4b45c1097729238d5ad4af54dc403daa8627d804
Henry Wang <henanwang@Henrys-MacBook-Pro.local>
2019-02-13 23:51:06 -0500

    first commit

---
+++ src/components/TopNavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../assets/images/Nbase2.png';
+
+export class TopNavBar extends React.Component {
+    render() {
+        return (
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        );
+    }
+}
